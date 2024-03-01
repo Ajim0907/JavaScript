01d0406c2b16204bd0255c5793779bdb795f44ad
Ajim kotwal <122783606+Ajim0907@users.noreply.github.com>
2024-03-01 19:24:19 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountMail = "ajim007@gmial.com"
+let accountPassword = "12345"
+accountCIty = "mumbai"
+let accountState;
+
+//accountId = 5678 you cannot assign revalue to const varible
+accountMail = "ajim0907@yahoo.com"
+accountPassword = 789456
+accountCIty = "delhi"
+
+console.table([accountMail,accountPassword,accountCIty,accountState])
+

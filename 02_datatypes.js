a6dc6d73f0ce50316b15cbc229db80c05cbecf1e
Ajim kotwal <122783606+Ajim0907@users.noreply.github.com>
2024-03-02 20:11:44 +0530

    today we learned about datatypes and ECMA Standard

---
+++ 02_datatypes.js
@@ -0,0 +1,28 @@
+//datatypes and ECMA pattern
+// "use strict" treat all JS code as newer version 
+
+let name = "ajim" //string
+let age = 23 //number
+isLoggedIN = false //boolean
+
+// there are two types of datatypes 1 ) primitive datatypes 2) non primitive datatypes
+// primitive data types 
+
+// number 
+// Name 
+// BigInt
+// boolean
+// null
+// undefined
+// symbol
+
+// object
+
+console.log(typeof undefined);  //undefined 
+console.log(typeof null);   // null
+
+
+
+
+
+

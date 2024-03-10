175158dbd7d3a0358d49e6b9917900011a9e7dcd
Ajim kotwal <122783606+Ajim0907@users.noreply.github.com>
2024-03-10 10:59:18 +0530

    Update 13_objects_2.js

--- 13_objects_2.js
+++ 13_objects_2.js
@@ -1,16 +1,7 @@
 // singleton objects
 
-// create obj
 
-const netflix = {}
 
-netflix.userName = "Ajim"
-netflix.id = 1
-netflix.Email = "kotwalajim007@gmail.com"
-netflix.loggedIn = false
-
-console.log(netflix.Email);
-console.log(netflix.id);
-console.log(netflix.loggedIn);
+;
 
 
